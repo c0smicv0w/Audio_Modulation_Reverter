5db7bd5950a401d3d2718d40f0bd0161b9614672
c0smicv0w <jang.hw73@gmail.com>
2016-01-10 21:56:40 +0900

    implement fast fourier transform

--- amr/Transform.cpp
+++ amr/Transform.cpp
@@ -1,4 +1,5 @@
 #include <math.h>
+#include <algorithm>
 #include "Transform.h"
 
 using namespace Transform;
@@ -43,4 +44,85 @@ Transform::DFT1d(double *re, double *im, int N, int dir)
 
     delete[] tr;
     delete[] ti;
+}
+
+Transform::FFT1d(double *re, double *im, int N, int dir)
+{
+    register int i, j, k;
+
+    //swap input data
+    int n2 = N >> 1;
+    int nb = 0;
+
+    while (N != (1 << nb))
+        nb++;
+
+    for (i = 0, j = 0; j < N - 1; i++)
+    {
+        if (i < j)
+        {
+            std::swap(re[i], re[j]);
+            std::swap(im[i], im[j]);
+        }
+
+        k = n2;
+
+        while (k <= j)
+        {
+            i -= k;
+            k >>= 1;
+        }
+
+        j+=k;
+    }
+
+    //butterfly algorithm
+    int i1, l, l1, l2;
+    double c1, c2, t1, t2, u1, u2, z;
+
+    c1 = -1.0;
+    c2 = 0.0;
+    l2 = 1;
+
+    for (l = 0; l < nb; l++)
+    {
+        l1 = l2;
+        l2 <<= 1;
+        u1 = 1.0;
+        u2 = 0.0;
+
+        for (j = 0; j < l1; j++)
+        {
+            for (i = j; i < N; i += 12)
+            {
+                i1 = i + l1;
+                t1 = u1 * re[i1] - u2 * im[i1];
+                t2 = u1 * im[i1] + u2 * re[i1];
+                re[i1] = re[i] - t1;
+                im[i1] = im[i] - t2;
+                re[i] += t1;
+                im[i] += t2;
+            }
+
+            z = u1 * c1 - u2 *c2;
+            u2 = u1 * c2 + u2 * c1;
+            u1 = z;
+        }
+
+        c2 = sqrt((1.0 - c1) / 2.0);
+
+        if (dir == 1) // forward
+            c2 = -c2;
+
+        c1 = sqrt((1.0 + c1) / 2.0);
+    }
+
+    if (dir == -1) // IDFT
+    {
+        for (i = 0; i < N; i++)
+        {
+            re[i] /= static_cast<double>(N);
+            im[i] /= static_cast<double>(N);
+        }
+    }
 }
--- amr/main.cpp
+++ amr/main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char* argv[])
         cout << "Usage: " << argv[0] << "inputfile outputfile [option]" << endl;
         return 0;
     }
-    cout << "Hello World!" << endl;
+
     return 0;
 }
 
